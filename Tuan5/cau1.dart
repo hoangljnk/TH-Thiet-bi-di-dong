d3e933cd5aec381a566b250ccbcfb27bcc96d3f8
hoangljnk <125437127+hoangljnk@users.noreply.github.com>
2023-03-15 09:08:34 +0700

    Update cau1.dart

--- Tuan5/cau1.dart
+++ Tuan5/cau1.dart
@@ -2,7 +2,7 @@ import 'dart:io';
 
 void main() {
   File file = File('hello.txt');
-  String Name = 'Nguyễn Thanh Bình';
+  String Name = 'Hoàng Thị Thùy Linh';
   file.writeAsStringSync('Hello ${Name}\n', mode: FileMode.append);
   print('Thêm tên thành công!');
 }
